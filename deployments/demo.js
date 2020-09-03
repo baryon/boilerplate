5c104f7c9f08efc30eec6a98b1675ec7e77c2ad8
lilong <lilong@gmail.com>
2020-09-03 18:59:13 +0900

    对demo的解析

--- deployments/demo.js
+++ deployments/demo.js
@@ -19,14 +19,26 @@ const { privateKey } = require('../privateKey');
         console.log('locking txid:     ', lockingTxid)
         
         // unlock
-        const unlockingScript = demo.add(11).toScript()
+        const unlockingFn = demo.add(11) //解锁成功
+        // const unlockingFn = demo.add(12) //抛出异常
+        // const unlockingFn = demo.sub(-3) //解锁成功
+        // const unlockingFn = demo.sub(3) //抛出异常
+        console.log(unlockingFn)
+        //可以事前校验，只有可以正常解锁在发送交易
+        console.log(unlockingFn.verify())
+
+        const unlockingScript = unlockingFn.toScript()
         const unlockingTx = await createUnlockingTx(lockingTxid, amount, demo.lockingScript.toASM(), newAmount)
         unlockingTx.inputs[0].setScript(unlockingScript)
         const unlockingTxid = await sendTx(unlockingTx)
         console.log('unlocking txid:   ', unlockingTxid)
-
         console.log('Succeeded on testnet')
-    } catch (error) {
+
+        //如果将期待结果改为12，则会出错，抛出异常
+        // const unlockingScript = demo.add(12).toScript()
+        // Failed - StatusCodeError: 400 - "16: mandatory-script-verify-flag-failed (Script evaluated without error but finished with a false/empty top stack element)"
+
+      } catch (error) {
         console.log('Failed on testnet')
         showError(error)
     }
--- tests/js/demo.scrypttest.js
+++ tests/js/demo.scrypttest.js
@@ -1,3 +1,9 @@
+// const console = require('tracer').colorConsole({
+//   // inspectOpt: {
+//   //   showHidden: true, //the object's non-enumerable properties will be shown too
+//   //   depth: null //tells inspect how many times to recurse while formatting the object. This is useful for inspecting large complicated objects. Defaults to 2. To make it recurse indefinitely pass null.
+//   // }
+// })
 const { expect } = require('chai');
 const { buildContractClass } = require('scryptlib');
 const { compileContract } = require('../../helper');
@@ -8,20 +14,47 @@ describe('Test sCrypt contract Demo In Javascript', () => {
 
   before(() => {
     const Demo = buildContractClass(compileContract('demo.scrypt'));
+    console.log('Demo',Demo)
+    // add和sub是公开函数
+    // abi: [
+    //   { type: 'function', name: 'add', index: 0, params: [Array] },
+    //   { type: 'function', name: 'sub', index: 1, params: [Array] },
+    //   { type: 'constructor', name: 'constructor', params: [Array] }
+    // ],
+    //7和4作为两个数字，分别被压入栈中
     demo = new Demo(7, 4);
+    console.log('demo',demo)
   });
 
   it('should return true', () => {
-    result = demo.add(7 + 4).verify()
+    //公开函数可以进行调用, addFn是一个函数调用，7+4的结果11是参数，进行函数调用
+    const addFn = demo.add(7 + 4)
+    console.log(addFn, addFn.unlockingScript.chunks)
+    //解锁脚本传入的是两个数字，12和1，第一个参数估计是参数7+4的结果11。第2参数1估计表示第一个函数，也就是add
+    result = addFn.verify()
+    console.log(result)
+    //result包含两个字段 success， true是成功，否则为false， error字段是出错原因，没出错则为空字符串
     expect(result.success, result.error).to.be.true
-    result = demo.sub(7 - 4).verify()
+    const subFn = demo.sub(7 - 4)
+    console.log(subFn, subFn.unlockingScript.chunks)
+    //解锁脚本传入的是两个数字，3和2，第一个参数估计是参数7-4的结果3。第2参数3估计表示第一个函数，也就是sub
+    result = subFn.verify()
+    console.log(result)
     expect(result.success, result.error).to.be.true
   });
 
   it('should throw error', () => {
-    result = demo.add(0).verify()
+    const addFn = demo.add(0)
+    console.log(addFn, addFn.unlockingScript.chunks)
+     //解锁脚本传入的是两个数字，0和1，第一个参数估计是参数0。第2参数1估计表示第一个函数，也就是add
+     result = addFn.verify()
+    console.log(result)
     expect(result.success, result.error).to.be.false
-    result = demo.sub(1).verify()
+    const subFn = demo.sub(1)
+    console.log(subFn, subFn.unlockingScript.chunks)
+    //解锁脚本传入的是两个数字，1和2，第一个参数估计是参数1。第2参数3估计表示第一个函数，也就是sub
+    result = subFn.verify()
+    console.log(result)
     expect(result.success, result.error).to.be.false
   });
 });
