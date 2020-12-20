c1aa6ed97971d835618a2a5fa989cfa2ce45cdf4
lilong <lilong@gmail.com>
2020-12-20 17:09:03 +0900

    fixed fetch bug on WoC

--- createScriptFromTx.js
+++ createScriptFromTx.js
@@ -1,48 +1,17 @@
-const bsv = require( 'bsv' )
+const { bsv, bin2num, num2bin } = require( 'scryptlib' )
 const WhatsOnChain = require( 'whatsonchain' )
 const _ = require( 'lodash' )
 
-function bnToHex ( bn ) {
-  var pos = true;
-  bn = BigInt( bn );
+async function createScriptFromTx ( network, prevTxid, outputIndex, unlockTxId, scriptName ) {
 
-  // I've noticed that for some operations BigInts can
-  // only be compared to other BigInts (even small ones).
-  // However, <, >, and == allow mix and match
-  if ( bn < 0 ) {
-    pos = false;
-    bn = bitnot( bn );
-  }
-
-  var base = 16;
-  var hex = bn.toString( base );
-  if ( hex.length % 2 ) {
-    hex = '0' + hex;
-  }
-
-  // Check the high byte _after_ proper hex padding
-  var highbyte = parseInt( hex.slice( 0, 2 ), 16 );
-  var highbit = ( 0x80 & highbyte );
-
-  if ( pos && highbit ) {
-    // A 32-byte positive integer _may_ be
-    // represented in memory as 33 bytes if needed
-    hex = '00' + hex;
-  }
-
-  return hex;
-}
-
-async function main ( network, prevTxid, outputIndex, unlockTxId, scriptName ) {
   const woc = new WhatsOnChain( network )
 
   //fetch transaction from prevTxid
-  const prevTxHex = await woc.getRawTxData( prevTxid )
+  const prevTx = await woc.txHash( prevTxid )
 
   //analyse locking script from transaction's outputIndex
-  const prevTx = new bsv.Transaction( prevTxHex )
-  const output = prevTx.outputs[ outputIndex ]
-  const asm = output.script.toASM()
+  const output = prevTx.vout[ outputIndex ]
+  const asm = ( new bsv.Script( output.scriptPubKey.hex ) ).toASM()
   const fixedAsm = _.map( asm.split( ' ' ), ( item ) => {
     switch ( item ) {
       case '0':
@@ -55,26 +24,35 @@ async function main ( network, prevTxid, outputIndex, unlockTxId, scriptName ) {
   } ).join( ' ' )
 
   //fetch transaction from unlocking tx
-  const unlockTxHex = await woc.getRawTxData( unlockTxId )
-  const unlockTx = new bsv.Transaction( unlockTxHex )
+  const unlockTx = await woc.txHash( unlockTxId )
   let params, unlockParams
-  for ( const input of unlockTx.inputs ) {
-    if ( input.outputIndex === outputIndex && input.prevTxId.toString( 'hex' ) === prevTxid ) {
-      const unlockASM = input.script.toASM()
-      const asms = unlockASM.split( ' ' )
+  for ( const input of unlockTx.vin ) {
+    if ( input.vout === outputIndex && input.txid === prevTxid ) {
+      const unlockASM = ( new bsv.Script( input.scriptSig.hex ) ).toASM()
+      const asms = _.map( unlockASM.split( ' ' ), ( item ) => {
+        switch ( item ) {
+          case '0':
+            return '00'
+          case '-1':
+            return '81'
+          default:
+            return item
+        }
+      } )
       params = 'bytes p0'
-      unlockParams = `"b'${bnToHex( BigInt( asms[ 0 ] ) )}'"`
+      unlockParams = `"b'${asms[ 0 ]}'"`
       for ( let i = 1; i < asms.length; i++ ) {
         params += `, bytes p${i}`
-        unlockParams += `, "b'${bnToHex( BigInt( asms[ 0 ] ) )}'"`
+        unlockParams += `, "b'${asms[ i ]}'"`
       }
 
     }
   }
-  //const output = prevTx.outputs[ outputIndex ]
+  if ( !unlockParams ) {
+    throw "Can't found unlock script"
+  }
 
   //create scrypt file
-
   const contract = `
 contract ${scriptName} {
   public function unlock(${params}) {
@@ -106,14 +84,18 @@ contract ${scriptName} {
   console.log( `launche.json ` + '>'.repeat( 10 ) )
   console.log( debugSetting )
   console.log( '<'.repeat( 10 ) + ` launche.json` )
+
+
+  return { contract, debugSetting }
 }
 
-
+module.exports = createScriptFromTx
 
 //network: testnet/livenet
 const network = 'livenet'
 
 //prevTxid
+//const prevTxid = '3bf81a80b3e75627ebac4b5e9ab638f8c9141579fadcb1340b28a81c20a17cfc'//'b6e38bd77d00ce007c139f0e71c9c9fbade801eaa2ffcb95fd27849df110e86a'
 const prevTxid = 'b6e38bd77d00ce007c139f0e71c9c9fbade801eaa2ffcb95fd27849df110e86a'
 
 //outputIndex
@@ -121,9 +103,9 @@ const outputIndex = 0
 
 //unlocking tx
 const unlockTxId = '20adad8bd4cc694cfed4ccadff911433601e55b0f8779e839bc6579cb8d234f9'
+// const unlockTxId = 'fc5e29ae7aafeb774cc1f94c951381bff358af64e8ad33a8a932c2271c64f2a4'//'20adad8bd4cc694cfed4ccadff911433601e55b0f8779e839bc6579cb8d234f9'
 
 //scriptName
 const scriptName = 'MultiSignBug'
 
-
-main( network, prevTxid, outputIndex, unlockTxId, scriptName )
+createScriptFromTx( network, prevTxid, outputIndex, unlockTxId, scriptName )
