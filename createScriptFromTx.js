6bf9edacc4c32141c435d7bbc290106ef610a818
lilong <lilong@gmail.com>
2020-12-21 10:49:08 +0900

    fetch tx from unlock outpoint

--- createScriptFromTx.js
+++ createScriptFromTx.js
@@ -2,9 +2,33 @@ const { bsv, bin2num, num2bin } = require( 'scryptlib' )
 const WhatsOnChain = require( 'whatsonchain' )
 const _ = require( 'lodash' )
 
-async function createScriptFromTx ( network, prevTxid, outputIndex, unlockTxId, scriptName ) {
+async function createScriptFromTx ( network, unlockTxId, inputIndex, scriptName ) {
 
   const woc = new WhatsOnChain( network )
+
+  //fetch transaction from unlocking tx
+  const unlockTx = await woc.txHash( unlockTxId )
+  const input = unlockTx.vin[ inputIndex ]
+  const prevTxid = input.txid
+  const outputIndex = input.vout
+  let params, unlockParams
+  const unlockASM = ( new bsv.Script( input.scriptSig.hex ) ).toASM()
+  const asms = _.map( unlockASM.split( ' ' ), ( item ) => {
+    switch ( item ) {
+      case '0':
+        return '00'
+      case '-1':
+        return '81'
+      default:
+        return item
+    }
+  } )
+  params = 'bytes p0'
+  unlockParams = `"b'${asms[ 0 ]}'"`
+  for ( let i = 1; i < asms.length; i++ ) {
+    params += `, bytes p${i}`
+    unlockParams += `, "b'${asms[ i ]}'"`
+  }
 
   //fetch transaction from prevTxid
   const prevTx = await woc.txHash( prevTxid )
@@ -22,35 +46,6 @@ async function createScriptFromTx ( network, prevTxid, outputIndex, unlockTxId, 
         return item
     }
   } ).join( ' ' )
-
-  //fetch transaction from unlocking tx
-  const unlockTx = await woc.txHash( unlockTxId )
-  let params, unlockParams
-  for ( const input of unlockTx.vin ) {
-    if ( input.vout === outputIndex && input.txid === prevTxid ) {
-      const unlockASM = ( new bsv.Script( input.scriptSig.hex ) ).toASM()
-      const asms = _.map( unlockASM.split( ' ' ), ( item ) => {
-        switch ( item ) {
-          case '0':
-            return '00'
-          case '-1':
-            return '81'
-          default:
-            return item
-        }
-      } )
-      params = 'bytes p0'
-      unlockParams = `"b'${asms[ 0 ]}'"`
-      for ( let i = 1; i < asms.length; i++ ) {
-        params += `, bytes p${i}`
-        unlockParams += `, "b'${asms[ i ]}'"`
-      }
-
-    }
-  }
-  if ( !unlockParams ) {
-    throw "Can't found unlock script"
-  }
 
   //create scrypt file
   const contract = `
@@ -94,18 +89,14 @@ module.exports = createScriptFromTx
 //network: testnet/livenet
 const network = 'livenet'
 
-//prevTxid
-//const prevTxid = '3bf81a80b3e75627ebac4b5e9ab638f8c9141579fadcb1340b28a81c20a17cfc'//'b6e38bd77d00ce007c139f0e71c9c9fbade801eaa2ffcb95fd27849df110e86a'
-const prevTxid = 'b6e38bd77d00ce007c139f0e71c9c9fbade801eaa2ffcb95fd27849df110e86a'
-
-//outputIndex
-const outputIndex = 0
-
 //unlocking tx
 const unlockTxId = '20adad8bd4cc694cfed4ccadff911433601e55b0f8779e839bc6579cb8d234f9'
-// const unlockTxId = 'fc5e29ae7aafeb774cc1f94c951381bff358af64e8ad33a8a932c2271c64f2a4'//'20adad8bd4cc694cfed4ccadff911433601e55b0f8779e839bc6579cb8d234f9'
+//const unlockTxId = 'fc5e29ae7aafeb774cc1f94c951381bff358af64e8ad33a8a932c2271c64f2a4'
+
+//outputIndex
+const inputIndex = 0
 
 //scriptName
 const scriptName = 'MultiSignBug'
 
-createScriptFromTx( network, prevTxid, outputIndex, unlockTxId, scriptName )
+createScriptFromTx( network, unlockTxId, inputIndex, scriptName )
